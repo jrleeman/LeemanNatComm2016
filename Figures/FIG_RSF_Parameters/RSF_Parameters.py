96e28b2dfe61a0e38ba920091cdf142b25918f99
John Leeman <kd5wxb@gmail.com>
2015-01-13 11:14:20 -0500

    Remove individual a,b panels in RSF plot

--- Figures/FIG_RSF_Parameters/RSF_Parameters.py
+++ Figures/FIG_RSF_Parameters/RSF_Parameters.py
@@ -22,67 +22,18 @@ data =  data.query('Grade == ["A","B"]')
 
 # Setup figure and axes
 # Generally plots is ~1.33x width to height (10,7.5 or 12,9)
-fig = plt.figure(figsize=(18,4.5))
-ax1 = fig.add_subplot(2, 3, 1)
-ax2 = fig.add_subplot(2, 3, 4)
-ax3 = fig.add_subplot(1, 3, 2)
-ax4 = fig.add_subplot(1, 3, 3)
-fig.subplots_adjust(hspace=0.1, wspace=0.35)
+fig = plt.figure(figsize=(17,8))
+ax3 = fig.add_subplot(1, 2, 1)
+ax4 = fig.add_subplot(1, 2, 2)
+#fig.subplots_adjust(hspace=0.1, wspace=0.35)
 
-
-# Left plot of a,b
-
-# Label Plot
-ax1.text(0.01,0.9,'A',transform = ax1.transAxes,fontsize=24)
-
-# Set labels and tick sizes
-ax1.set_xlabel(r'',fontsize=18)
-ax1.set_ylabel(r'a',fontsize=18)
-ax1.tick_params(axis='both', which='major', labelsize=16)
-ax1.xaxis.set_ticklabels([])
-
-ax2.set_xlabel(r'Displacement [mm]',fontsize=18)
-ax2.set_ylabel(r'b',fontsize=18)
-ax2.tick_params(axis='both', which='major', labelsize=16)
-
-# Turns off chart clutter
-
-# Turn off top and right tick marks
-ax1.get_xaxis().tick_bottom()
-ax1.get_yaxis().tick_left()
-ax2.get_xaxis().tick_bottom()
-ax2.get_yaxis().tick_left()
-
-# Turn off top and right splines
-ax1.spines["top"].set_visible(False)
-ax1.spines["right"].set_visible(False)
-ax2.spines["top"].set_visible(False)
-ax2.spines["right"].set_visible(False)
-
-# Plotting RSF Parameter a
-up = data[data['Type']=='Up']
-ax1.scatter(up['LP_Disp']/1000,up['a'],color=tableau20[0],
-            s=50,marker='^', label='Velocity Step Up')
-
-down = data[data['Type']=='Down']
-ax1.scatter(down['LP_Disp']/1000,down['a'],color=tableau20[1],
-            s=50,marker='v', label='Velocity Step Down')
-
-# Plotting RSF Parameter b
-up = data[data['Type']=='Up']
-ax2.scatter(up['LP_Disp']/1000,up['b'],color=tableau20[2],
-            s=50,marker='^', label='Velocity Step Up')
-
-down = data[data['Type']=='Down']
-ax2.scatter(down['LP_Disp']/1000,down['b'],color=tableau20[3],
-            s=50,marker='v', label='Velocity Step Down')
 
 #
-# Middle Plot of a-b
+# Left Plot of a-b
 #
 
 # Label Plot
-ax3.text(0.01,0.9,'B',transform = ax3.transAxes,fontsize=24)
+ax3.text(0.01,0.9,'A',transform = ax3.transAxes,fontsize=24)
 
 # Set labels and tick sizes
 ax3.set_xlabel(r'Displacement [mm]',fontsize=18)
@@ -114,12 +65,14 @@ ax3.axhline(y=0,color='k',linewidth='2',linestyle='--')
 ax3.text(15,0.0005,'Velocity Strengthening',fontsize=10)
 ax3.text(16,-0.0007,'Velocity Weakening',fontsize=10)
 
+ax3.set_ylim(-0.006 ,0.006)
+
 #
 # Right Plot of Dc
 #
 
 # Label Plot
-ax4.text(0.01,0.9,'C',transform = ax4.transAxes,fontsize=24)
+ax4.text(0.01,0.9,'B',transform = ax4.transAxes,fontsize=24)
 
 # Set labels and tick sizes
 ax4.set_xlabel(r'Displacement [mm]',fontsize=18)
@@ -144,5 +97,7 @@ ax4.scatter(up['LP_Disp']/1000,up['Dc'],color=tableau20[4],
 down = data[data['Type']=='Down']
 ax4.scatter(down['LP_Disp']/1000,down['Dc'],color=tableau20[5],
             s=50,marker='v', label='Velocity Step Down')
+
+ax4.set_ylim(0.0,35)
 
 plt.savefig('RSF_Parameters.png', bbox_inches="tight")
