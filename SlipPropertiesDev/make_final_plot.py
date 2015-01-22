f2849c6ca03909ab8f85e0ab73e711c007f142a1
John Leeman <kd5wxb@gmail.com>
2015-01-22 18:04:04 -0500

    Update rainbow plot with annotations

--- SlipPropertiesDev/make_final_plot.py
+++ SlipPropertiesDev/make_final_plot.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def load_event_properties(experiment):
     """
@@ -40,6 +41,12 @@ def filter(data,col,low,high):
 experiments_with_event_data = ('p4342','p4343','p4344','p4345','p4346',
                                'p4347','p4348','p4350','p4351')
 
+# Tuple of experiments we'll plot unload/reload stiffness from
+experiments_with_unload_reload = ('p4267','p4268','p4269','p4270','p4271',
+                                  'p4272','p4273','p4309','p4310','p4311',
+                                  'p4312','p4313','p4314','p4316','p4317',
+                                  'p4327','p4328','p4329','p4330')
+
 # Read those experiments into a dictionary of event data
 experiment_event_data = dict()
 
@@ -49,21 +56,21 @@ for experiment in experiments_with_event_data:
 # Make the plot
 # Setup figure and axes
 # Generally plots is ~1.33x width to height (10,7.5 or 12,9)
-fig = plt.figure(figsize=(12,9))
+fig = plt.figure(figsize=(12,12))
 ax1 = plt.subplot(2,1,1)
 ax2 = plt.subplot(2,2,3)
 ax3 = plt.subplot(2,2,4)
 
 # Set labels and tick sizes
-ax1.set_xlabel(r'Load Point Displacement [$\mu m$]',fontsize=18)
+ax1.set_xlabel(r'Load Point Displacement [$\mu m$]',fontsize=18,labelpad=15)
 ax1.set_ylabel(r'Stiffness [1/$\mu m$]',fontsize=18)
 ax1.tick_params(axis='both', which='major', labelsize=16)
 
-ax2.set_xlabel(r'Stiffness [1/$\mu m$]',fontsize=18)
+ax2.set_xlabel(r'Stiffness [1/$\mu m$]x10000',fontsize=18,labelpad=15)
 ax2.set_ylabel(r'Peak Slip Velocity [$\mu m/s$]',fontsize=18)
 ax2.tick_params(axis='both', which='major', labelsize=16)
 
-ax3.set_xlabel(r'Stiffness [1/$\mu m$]',fontsize=18)
+ax3.set_xlabel(r'Stiffness [1/$\mu m$]x10000',fontsize=18,labelpad=15)
 ax3.set_ylabel(r'Slip Duration [$s$]',fontsize=18)
 ax3.tick_params(axis='both', which='major', labelsize=16)
 
@@ -92,6 +99,8 @@ ax3.spines["right"].set_visible(False)
 # Plotting
 
 # Make panel A of displacement/stiffness
+ax1.text(0.01,0.9,'A',transform = ax1.transAxes,fontsize=24)
+
 low_color = 0.
 high_color = 1000.
 color_map = plt.get_cmap('jet')
@@ -105,6 +114,10 @@ for key in experiment_event_data:
 
 # cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
 # plt.colorbar(sc,cax=cbar_ax)
+# for experiment in experiments_with_unload_reload:
+#     df = pd.read_csv('/Users/jleeman/Dropbox/PennState/BiaxExperiments/%s/%s_stiffness_cycles.txt'%(experiment,experiment))
+#
+#     ax1.scatter(df['AvgDisp']/1000.,df['Slope'],color='g',s=50,alpha=0.6)
 
 ax1.set_ylim(0,0.0009)
 ax1.set_xlim(8,52)
@@ -112,6 +125,7 @@ ax1.set_xlim(8,52)
 ax1.axvspan(40, 50, alpha=0.2, color='k', zorder=0)
 
 # Panel B
+ax2.text(0.01,0.9,'B',transform = ax2.transAxes,fontsize=24)
 
 filter_col = 9
 low_val = 40000.
@@ -122,14 +136,14 @@ for key in experiment_event_data:
     event_data = experiment_event_data[key]
     event_data = filter(event_data,filter_col,low_val,high_val)
 
-    ax2.scatter(event_data[:,5],event_data[:,y_col],color='k',alpha=marker_alpha)
-    ax2.errorbar(np.mean(event_data[:,5]),np.mean(event_data[:,y_col]),fmt='ro',ecolor='k',elinewidth=2,xerr=np.std(event_data[:,5]),yerr=np.std(event_data[:,y_col]))
+    ax2.scatter(event_data[:,5]*10000,event_data[:,y_col],color='k',alpha=marker_alpha)
+    ax2.errorbar(np.mean(event_data[:,5]*10000),np.mean(event_data[:,y_col]),fmt='ro',ecolor='k',elinewidth=2,xerr=np.std(event_data[:,5]*10000),yerr=np.std(event_data[:,y_col]))
 
 ax2.set_ylim(0,900)
-ax2.set_xlim(0.00045,0.00085)
-ax2.set_xticks([0.00045,0.00055,0.00065,0.00075,0.00085])
+ax2.set_xlim(4.5,8.5)
 
 # Panel C
+ax3.text(0.01,0.9,'C',transform = ax3.transAxes,fontsize=24)
 
 filter_col = 9
 low_val = 40000.
@@ -140,11 +154,10 @@ for key in experiment_event_data:
     event_data = experiment_event_data[key]
     event_data = filter(event_data,filter_col,low_val,high_val)
 
-    ax3.scatter(event_data[:,5],event_data[:,y_col],color='k',alpha=marker_alpha)
-    ax3.errorbar(np.mean(event_data[:,5]),np.mean(event_data[:,y_col]),fmt='ro',ecolor='k',elinewidth=2,xerr=np.std(event_data[:,5]),yerr=np.std(event_data[:,y_col]))
+    ax3.scatter(event_data[:,5]*10000,event_data[:,y_col],color='k',alpha=marker_alpha)
+    ax3.errorbar(np.mean(event_data[:,5]*10000),np.mean(event_data[:,y_col]),fmt='ro',ecolor='k',elinewidth=2,xerr=np.std(event_data[:,5]*10000),yerr=np.std(event_data[:,y_col]))
 
 ax3.set_ylim(0,1.2)
-ax3.set_xlim(0.00045,0.00085)
-ax3.set_xticks([0.00045,0.00055,0.00065,0.00075,0.00085])
+ax3.set_xlim(4.5,8.5)
 
 plt.show()
