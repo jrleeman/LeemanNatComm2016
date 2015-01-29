e7e469dbe49eec05fb1118114253582c9e6dcbdb
John Leeman <kd5wxb@gmail.com>
2015-01-29 13:30:33 -0500

    Change units to mm/s

--- Figures/Fig_Slip_Properties/make_final_plot.py
+++ Figures/Fig_Slip_Properties/make_final_plot.py
@@ -159,7 +159,7 @@ ax1.set_ylabel(r'Stiffness [1/$\mu m$]',fontsize=18)
 ax1.tick_params(axis='both', which='major', labelsize=16)
 
 ax2.set_xlabel(r'Stiffness [1/$\mu m$]x10000',fontsize=18,labelpad=15)
-ax2.set_ylabel(r'Peak Slip Velocity [$\mu m/s$]',fontsize=18)
+ax2.set_ylabel(r'Peak Slip Velocity [$mm/s$]',fontsize=18)
 ax2.tick_params(axis='both', which='major', labelsize=16)
 
 ax3.set_xlabel(r'Stiffness [1/$\mu m$]x10000',fontsize=18,labelpad=15)
@@ -193,8 +193,8 @@ ax3.spines["right"].set_visible(False)
 # Make panel A of displacement/stiffness
 ax1.text(0.01,0.9,'B',transform = ax1.transAxes,fontsize=24)
 
-low_color = 10.
-high_color = 4000.
+low_color = 10./1000.
+high_color = 4000./1000.
 color_map = plt.get_cmap('YlOrRd')
 marker_size = 40
 marker_alpha=0.5
@@ -202,7 +202,7 @@ color_col=11
 
 for key in experiment_event_data:
     event_data = experiment_event_data[key]
-    sc = ax1.scatter(event_data[:,9]/1000.,event_data[:,5],c=event_data[:,color_col],s=marker_size,alpha=marker_alpha,vmin=low_color,vmax=high_color,cmap=color_map)
+    sc = ax1.scatter(event_data[:,9]/1000.,event_data[:,5],c=event_data[:,color_col]/1000.,s=marker_size,alpha=marker_alpha,vmin=low_color,vmax=high_color,cmap=color_map)
     print key,np.min(event_data[:,color_col]), np.max(event_data[:,color_col])
 # cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
 # plt.colorbar(sc,cax=cbar_ax)
@@ -214,7 +214,7 @@ for key in experiment_event_data:
 position=fig.add_axes([0.37,0.42,0.5,0.02])  ## the parameters are the specified position you set [left, bottom, width, height]
 cb = fig.colorbar(sc,cax=position,orientation='horizontal')
 cb.solids.set_edgecolor("face")
-cb.set_label(r'Peak Slip Velocity [$\mu m/s$]',fontsize=14)
+cb.set_label(r'Peak Slip Velocity [$mm/s$]',fontsize=14)
 
 ax1.set_ylim(0,0.0009)
 ax1.set_xlim(8,52)
@@ -233,10 +233,10 @@ for key in experiment_event_data:
     event_data = experiment_event_data[key]
     event_data = filter(event_data,filter_col,low_val,high_val)
 
-    ax2.scatter(event_data[:,5]*10000,event_data[:,y_col],color='k',alpha=marker_alpha)
-    ax2.errorbar(np.mean(event_data[:,5]*10000),np.mean(event_data[:,y_col]),fmt='ro',ecolor='k',elinewidth=2,xerr=np.std(event_data[:,5]*10000),yerr=np.std(event_data[:,y_col]))
+    ax2.scatter(event_data[:,5]*10000,event_data[:,y_col]/1000.,color='k',alpha=marker_alpha)
+    ax2.errorbar(np.mean(event_data[:,5]*10000),np.mean(event_data[:,y_col]/1000.),fmt='ro',ecolor='k',elinewidth=2,xerr=np.std(event_data[:,5]*10000),yerr=np.std(event_data[:,y_col]/1000.))
 
-ax2.set_ylim(0,4000)
+ax2.set_ylim(0,4)
 ax2.set_xlim(4.5,8.5)
 
 # Panel C
