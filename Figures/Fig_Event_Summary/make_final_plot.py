7f858b0a938ffb830af803a42cf5f8702a1abcf1
John Leeman <kd5wxb@gmail.com>
2015-02-02 12:16:01 -0500

    Scale plot to k/kc

--- Figures/Fig_Event_Summary/make_final_plot.py
+++ Figures/Fig_Event_Summary/make_final_plot.py
@@ -77,7 +77,7 @@ ax1.get_yaxis().tick_left()
 ax1.spines["top"].set_visible(False)
 ax1.spines["right"].set_visible(False)
 
-ax1.text(-0.2,0.9,'A',transform = ax1.transAxes,fontsize=24)
+ax1.text(-0.2,0.95,'A',transform = ax1.transAxes,fontsize=24)
 
 filter_col = 9
 low_val = 40000.
@@ -88,11 +88,11 @@ for key in experiment_event_data:
     event_data = experiment_event_data[key]
     event_data = filter(event_data,filter_col,low_val,high_val)
 
-    ax1.scatter(event_data[:,5]*10000,event_data[:,y_col]/1000.,color='k',alpha=marker_alpha)
-    ax1.errorbar(np.mean(event_data[:,5]*10000),np.mean(event_data[:,y_col]/1000.),fmt='ro',ecolor='k',elinewidth=2,xerr=np.std(event_data[:,5]*10000),yerr=np.std(event_data[:,y_col]/1000.))
+    ax1.scatter(event_data[:,5]/0.0007,event_data[:,y_col]/1000.,color='k',alpha=marker_alpha)
+    ax1.errorbar(np.mean(event_data[:,5]/0.0007),np.mean(event_data[:,y_col]/1000.),fmt='ro',ecolor='k',elinewidth=2,xerr=np.std(event_data[:,5]/0.0007),yerr=np.std(event_data[:,y_col]/1000.))
 
 ax1.set_ylim(0,4)
-ax1.set_xlim(4.5,8.5)
+#ax1.set_xlim(4.5,8.5)
 
 # Panel B
 
@@ -112,7 +112,7 @@ ax2.spines["top"].set_visible(False)
 ax2.spines["right"].set_visible(False)
 
 
-ax2.text(-0.2,0.9,'B',transform = ax2.transAxes,fontsize=24)
+ax2.text(-0.2,0.95,'B',transform = ax2.transAxes,fontsize=24)
 
 filter_col = 9
 low_val = 40000.
@@ -123,10 +123,10 @@ for key in experiment_event_data:
     event_data = experiment_event_data[key]
     event_data = filter(event_data,filter_col,low_val,high_val)
 
-    ax2.scatter(event_data[:,5]*10000,event_data[:,y_col],color='k',alpha=marker_alpha)
-    ax2.errorbar(np.mean(event_data[:,5]*10000),np.mean(event_data[:,y_col]),fmt='ro',ecolor='k',elinewidth=2,xerr=np.std(event_data[:,5]*10000),yerr=np.std(event_data[:,y_col]))
+    ax2.scatter(event_data[:,5]/0.0007,event_data[:,y_col],color='k',alpha=marker_alpha)
+    ax2.errorbar(np.mean(event_data[:,5]/0.0007),np.mean(event_data[:,y_col]),fmt='ro',ecolor='k',elinewidth=2,xerr=np.std(event_data[:,5]/0.0007),yerr=np.std(event_data[:,y_col]))
 
 ax2.set_ylim(0,1.2)
-ax2.set_xlim(4.5,8.5)
+#ax2.set_xlim(4.5,8.5)
 
 plt.savefig('figure.png', bbox_inches="tight");
